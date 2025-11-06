283ddb6b38fcaae2d730be56debaa44bfbd2f9f9
kemezrpcz <tgergo0421@gmail.com>
2025-11-06 18:39:49 +0100

    Project files added. Task 2 created.

---
+++ Java_Eloadas_Spring_Boot/src/main/java/com/example/java_eloadas_spring_boot/JavaEloadasSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_eloadas_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEloadasSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEloadasSpringBootApplication.class, args);
+    }
+
+}
