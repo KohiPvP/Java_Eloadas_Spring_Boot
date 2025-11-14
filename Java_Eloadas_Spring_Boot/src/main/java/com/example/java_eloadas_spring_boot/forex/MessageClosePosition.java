bac24f8c7c11378b3c1cdc9e31750325fd05dc57
Krisz <torokkrisztofer46@gmail.com>
2025-11-14 20:42:19 +0100

    feat: add close_position controller(task9)

---
+++ Java_Eloadas_Spring_Boot/src/main/java/com/example/java_eloadas_spring_boot/forex/MessageClosePosition.java
@@ -0,0 +1,13 @@
+package com.example.java_eloadas_spring_boot.forex;
+
+public class MessageClosePosition {
+    private int tradeId;
+
+    public int getTradeId() {
+        return tradeId;
+    }
+
+    public void setTradeId(int tradeId) {
+        this.tradeId = tradeId;
+    }
+}
